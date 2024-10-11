20b4708e0ff44380d29bc52a1bead2f5c29c3bc3
QuangVu-dev <tqvu97@gmail.com>
2024-10-12 01:07:33 +0700

    add

---
+++ src/models/connect.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize("node44", "root", "123456", {
+   host: "localhost", // tên database
+   port: "3306", //tên user
+   dialect: "mysql", //password user
+});
+
+export default sequelize;
